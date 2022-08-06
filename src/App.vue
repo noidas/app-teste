0e35d160ecea4bc83f4322c50bcfa867cad7a72b
Adolfo <azaguini@gmail.com>
2022-08-06 18:11:59 -0300

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,28 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view path="$router.key" />
-    </v-main>
-  </v-app>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-  export default {
-    name:'App',
+import HelloWorld from './components/HelloWorld.vue'
 
-    data () {
-      return {
-      }
-    },
-
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
   }
+}
 </script>
 
-<style lang="scss">
-  .v-breadcrumbs__divider,
-  .v-breadcrumbs__item {
-    font-size: 1.3rem;
-    color: white !important;
-  }
-
-  a {
-    text-decoration: none;
-  }
-
-  // #app {
-  // }
-
-  // #nav {
-  //   padding: 30px;
-
-  //   a {
-  //     font-weight: bold;
-  //     color: #2c3e50;
-
-  //     &.router-link-exact-active {
-  //       color: #42b983;
-  //     }
-  //   }
-  // }
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
